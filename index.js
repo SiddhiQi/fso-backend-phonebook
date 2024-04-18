10eabaec8effaac584a39850f1be5934729bbdf3
SiddhiQi <kim.siddiqi@gmail.com>
2024-04-18 14:02:38 +0200

    exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
+morgan.token("data", (req, res) => (req.method === "POST" ? JSON.stringify(req.body) : " "))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 app.use(express.json())
 
 let persons = [
