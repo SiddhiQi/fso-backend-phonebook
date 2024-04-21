20ae0c1649a865488522b7f5a23644cafd10d9a2
SiddhiQi <kim.siddiqi@gmail.com>
2024-04-21 17:47:41 +0200

    - added frontend dist

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const cors = require("cors")
 const morgan = require("morgan")
 
-app.use(cors())
+app.use(express.static("dist"))
 app.use(express.json())
+app.use(cors())
 
 morgan.token("data", (req, res) => (req.method === "POST" ? JSON.stringify(req.body) : " "))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
